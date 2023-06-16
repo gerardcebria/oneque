9b133c59de739c0eefb9697b4c538748f177325c
Gerard Cebria <gerard.cebrialeon@luminis.eu>
2023-06-16 09:21:15 +0200

    added qr code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <QRComponent/>
   <EventDetail/>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import EventDetail from './components/EventDetail.vue'
+import QRComponent from './components/QRComponent.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    EventDetail
-  }
+    EventDetail,
+    QRComponent
+}
 }
 </script>
 
---
+++ src/components/QRComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <canvas ref="qrCode"></canvas>
+    </div>
+</template>
+<script>
+
+
+import QRious from 'qrious';
+
+export default {
+    data() {
+        return {
+            url: 'https://www.ejemplo.com', // URL que quieres convertir en QR
+            qr: null, // Instancia de QRious
+        };
+    },
+    mounted() {
+        this.generateQR();
+    },
+    methods: {
+        generateQR() {
+            this.qr = new QRious({
+                element: this.$refs.qrCode, // Referencia a un elemento HTML donde se mostrará el QR
+                value: this.url, // URL que se convertirá en QR
+                size: 200, // Tamaño del QR en píxeles
+            });
+        },
+    },
+};
+
+</script>
